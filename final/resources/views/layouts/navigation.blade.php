009ef0320ff1aa6ec0f489cd6d845bd6b9bbda80
Francisco Castillo <fcastillocorpus@gmail.com>
2025-05-22 00:12:33 -0600

    nav-bar style

--- final/resources/views/layouts/navigation.blade.php
+++ final/resources/views/layouts/navigation.blade.php
@@ -1,24 +1,25 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class=" border-b border-[#a6a15e] bg-[#60492C]">
   <!-- Primary Navigation Menu -->
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex justify-between h-16">
 
       <!-- Logo + Tabs -->
-      <div class="flex">
+      <div class="flex ">
         <!-- Logo -->
         <div class="shrink-0 flex items-center">
           <a href="{{ route('recipes.index') }}">
-            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+            <x-application-logo class="block h-9 w-auto fill-current text-red-800" />
           </a>
         </div>
 
         <!-- Recetas / Planes -->
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex ">
             @if (Auth::user()->role === 'admin')
             
               <x-nav-link
                 :href="route('recipes.index')"
                 :active="request()->routeIs('recipes.*')"
+                class="text-white"
               >
                 Recetas
               </x-nav-link>
@@ -27,6 +28,7 @@
           <x-nav-link
             :href="route('plans.index')"
             :active="request()->routeIs('plans.*')"
+            class="text-white"
           >
             Planes
           </x-nav-link>
