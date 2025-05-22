da7d7584db256e9d2c02384f0a406e321e802539
Francisco Castillo <fcastillocorpus@gmail.com>
2025-05-21 23:12:40 -0600

    login/register styled

--- final/resources/views/auth/forgot-password.blade.php
+++ final/resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="mb-4 text-sm text-white">
+        {{ __('¿Olvidaste tu contraseña? No hay problema. Solo danos tu email y te mandaremos un correo con un link para reestablecer tu contraseña.') }}
     </div>
 
     <!-- Session Status -->
@@ -17,8 +17,8 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+            <x-primary-button class="bg-[#463F1A]">
+                {{ __('Enviar link') }}
             </x-primary-button>
         </div>
     </form>
--- final/resources/views/components/text-input.blade.php
+++ final/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-[#463F1A] focus:ring-[#463F1A] rounded-md shadow-sm']) }}>
